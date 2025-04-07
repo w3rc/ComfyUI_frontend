ede9e1ac5c8312baced63e14d031d83ab8fc84fa
rupayan <rupayan@insynkstudios.com>
2025-04-07 18:52:19 +0530

    Update package version to 0.2.0 and remove obsolete egg-info files

--- comfyui_frontend_package/setup.py
+++ comfyui_frontend_package/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fs-comfyui-frontend-package",
-    version=os.getenv("COMFYUI_FRONTEND_VERSION") or "0.1.0",
+    version=os.getenv("COMFYUI_FRONTEND_VERSION") or "0.2.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[],
